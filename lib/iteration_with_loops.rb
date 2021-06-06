099e05794a5f634f59b4b46b9225b230d3cd4ae3
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-06-06 16:05:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,19 +3,20 @@ def find_even_values(src)
   #
   # Output all even values in each nested array
 
-array_of_arrays = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
-]
-
-p array_of_arrays[0][1]
- # => 2
-p array_of_arrays[1][0]
- # => 4
-p array_of_arrays[1][2]
- # => 6
-p array_of_arrays[2][1]
- # => 8
+if array_of_arrays[0][0].even?
+ p array_of_arrays[0][0]
+end
+if array_of_arrays[0][1].even?
+ p array_of_arrays[0][1]
+end
+if array_of_arrays[0][2].even?
+ p array_of_arrays[0][2]
+end
+if array_of_arrays[1][0].even?
+ p array_of_arrays[1][0]
+end
+if array_of_arrays[1][1].even?
+ p array_of_arrays[1][1]
+end
  
 end
